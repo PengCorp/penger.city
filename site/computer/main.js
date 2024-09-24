3d8e98385f04f1d8757182ff5c76bd0f0c27fd9f
jack <jack>
2024-09-24 20:15:20 +0000

    Add computer

---
+++ site/computer/main.js
@@ -0,0 +1,86 @@
+(function()
+{
+    function create(name)
+    {
+        let content;
+        let template = document.querySelector("template[name=\"" + name + "\"]");
+        
+        if (template)
+        {
+            content = template.content.cloneNode(true /* deep clone */);
+
+            if (template.hasAttribute("rooted"))
+            {
+                content = content.firstElementChild;
+            }
+
+            let boundTags = content.querySelectorAll("[id]");
+            for (let tag of boundTags)
+            {
+                content[tag.id] = tag;
+                tag.removeAttribute("id");
+            }
+        }
+        else
+        {
+            content = document.createElement(name);
+        }
+        
+        return content;
+    }
+
+    function onKey(event)
+    {
+        let modifier = event.altKey || event.ctrlKey;
+        let input = prompt.input;
+
+        if (event.code == "Backspace")
+        {
+            input.innerText = input.innerText.substring(0, input.innerText.length - 1);
+        }
+        else if (event.code == "Enter")
+        {
+            let output = submit(input.innerText);
+            addPrompt(output);
+        }
+        else if (event.key.length == 1 && !modifier)
+        {
+            input.innerText += event.key;
+            
+            event.preventDefault();
+        }
+
+        scroll();
+    }
+
+    function scroll()
+    {
+        let offset = screenContents.clientHeight - (document.getElementById("screen").clientHeight - 40);
+
+        if (offset > 0)
+        {
+            screenContents.style.top = "-" + offset + "px";
+        }
+    }
+
+    function addPrompt(output)
+    {
+        response = create("p")
+        response.innerText = output;
+        screenContents.appendChild(response);
+
+        if (prompt !== null)
+        {
+            prompt.removeChild(prompt.cursor);
+        }
+
+        prompt = create("prompt")
+        screenContents.appendChild(prompt);
+    }
+
+    let prompt = null;
+
+    addPrompt(startup());
+
+    addEventListener("keydown", onKey);
+})();
---
+++ site/computer/pengos.js
@@ -0,0 +1,9 @@
+function startup()
+{
+    return "Welcome to PengOS 2.1";
+}
+
+function submit(input)
+{
+    return "Hello, " + input + "!";
+}
