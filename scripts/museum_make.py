bdbfb6ae6142b126c836ff771ebbdb226f7cb0e9
anko <anko>
2024-10-25 13:11:16 +0000

    improved musuem page functionality

--- scripts/museum_make.py
+++ scripts/museum_make.py
@@ -6,15 +6,18 @@ def collect_images_with_resize(directory):
     remove_path = directory + '/'
     replace_path_with = 'pengers/'
     image_extensions = ['*.png', '*.gif']
-    
+
     image_files = []
-    
+
     for ext in image_extensions:
         for image_path in glob.glob(os.path.join(directory, ext)):
-            image_info = {"original": replace_path_with + image_path.replace(remove_path, ''), "resized": None}
-            
+            image_info = {
+                "original": replace_path_with + image_path.replace(remove_path, ''),
+                "emoji_name": ":" + os.path.basename(image_path).split(".")[0] + ":"
+            }
+
             image_files.append(image_info)
-    
+
     return image_files
 
 directory = 'site/museum/pengers'
@@ -22,7 +25,7 @@ images_with_resizes = collect_images_with_resize(directory)
 
 env = Environment(loader=FileSystemLoader('.'))  # The current directory
 template = env.get_template('./scripts/museum_template.html')
-    
+
 output_html = template.render({
     "images": images_with_resizes
 })
