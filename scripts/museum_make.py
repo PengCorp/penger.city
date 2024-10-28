a32a179c08fc10ff04f1fd74a0fcebb5fa559a69
strawberry <strawberry>
2024-10-28 06:10:43 +0000

    Add Preact, spicier Museum management

--- scripts/museum_make.py
+++ scripts/museum_make.py
@@ -2,7 +2,7 @@ import os
 import glob
 from jinja2 import Environment, FileSystemLoader
 
-def collect_images_with_resize(directory):
+def collect_images(directory):
     remove_path = directory + '/'
     replace_path_with = 'pengers/'
     image_extensions = ['*.png', '*.gif']
@@ -13,7 +13,9 @@ def collect_images_with_resize(directory):
         for image_path in glob.glob(os.path.join(directory, ext)):
             image_info = {
                 "original": replace_path_with + image_path.replace(remove_path, ''),
-                "emoji_name": ":" + os.path.basename(image_path).split(".")[0] + ":"
+                "emoji_name": ":" + os.path.basename(image_path).split(".")[0] + ":",
+                "name": os.path.basename(image_path).split(".")[0],
+                "file_name": os.path.basename(image_path)
             }
 
             image_files.append(image_info)
@@ -21,7 +23,16 @@ def collect_images_with_resize(directory):
     return image_files
 
 directory = 'site/museum/pengers'
-images_with_resizes = collect_images_with_resize(directory)
+images_with_resizes = collect_images(directory)
+images_with_resizes.sort(key=lambda x: x['name'])
+
+# for i in images_with_resizes:
+#     print({
+#         'name': i['name'],
+#         'file_name': i['file_name'],
+#         'emoji_name': i['emoji_name'],
+#     })
+# exit(0)
 
 env = Environment(loader=FileSystemLoader('.'))  # The current directory
 template = env.get_template('./scripts/museum_template.html')
