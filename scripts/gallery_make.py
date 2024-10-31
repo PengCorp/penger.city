4d3b9ddbe63ed3086ad6926b1c7ce1f694ae05eb
anko <anko>
2024-10-31 20:34:02 +0000

    gallery improvements

--- scripts/gallery_make.py
+++ scripts/gallery_make.py
@@ -1,38 +1,39 @@
 import os
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image
 import glob
 
 # Function to find all image files in a directory (recursively)
 def find_images_with_dirs(directory):
     supported_extensions = ('.png', '.jpg', '.jpeg', '.gif')
     images_with_dirs = {}
-    
+
     for root, _, files in os.walk(directory):
         relative_path = os.path.relpath(root, directory)
         images = []
-        
+
         for file in files:
             if file.lower().endswith(supported_extensions):
                 images.append(file)
-        
+
         if images:
-            images_with_dirs[relative_path] = ['/pengers/' + os.path.join(relative_path, img) for img in images]
-    
+            images_with_dirs[relative_path] = [ { "path": '/pengers/' + os.path.join(relative_path, img), "size": Image.open(os.path.join(directory, relative_path, img)) } for img in images]
+
     return images_with_dirs
 
 def collect_museum_images(directory):
     remove_path = directory + '/'
     replace_path_with = '../museum/pengers/'
     image_extensions = ['*.png', '*.gif']
-    
+
     image_files = []
-    
+
     for ext in image_extensions:
         for image_path in glob.glob(os.path.join(directory, ext)):
             image_info = {"original": replace_path_with + image_path.replace(remove_path, ''), "resized": None}
-            
+
             image_files.append(image_info)
-    
+
     return image_files
 
 # Directory to search for images
